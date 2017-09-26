e70aac9efc79cac7e8ea8f2af60a27b681315789
georlun <georlun@gmail.com>
2017-09-26 14:06:29 +0800

    reset db

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   //local database
-  'url' : 'mongodb://aapladmin:944968@mongodb/aaplapp'
+  //'url' : 'mongodb://aapladmin:944968@mongodb/aaplapp'
   //cloud database
-  //'url' : 'mongodb://aapladmin:944968@ds149743.mlab.com:49743/aaplapp'
+  'url' : 'mongodb://aapladmin:944968@ds149743.mlab.com:49743/aaplapp'
 };

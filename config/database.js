5d9bed808907465ec8ce895c63d482eec23b3d50
georlun <georlun@gmail.com>
2017-10-30 13:13:44 +0800

    add prod db
    Add link to production db

--- config/database.js
+++ config/database.js
@@ -3,6 +3,8 @@
 module.exports = {
   //local database
   //'url' : 'mongodb://localhost:27017/aaplapp'
-  //cloud database
+  //development cloud database
   'url' : 'mongodb://aapladmin:944968@ds149743.mlab.com:49743/aaplapp'
+  //production cloud database
+  //'url' : 'mongodb://aapladmin:admin3010aapl@ds141155-a0.mlab.com:41155,ds141155-a1.mlab.com:41155/aaplapp?replicaSet=rs-ds141155'
 };
